23c012eac0b23895ad72502d242c9fc7e7b08419
fereshtehbaradaran <bara.frsht@gmail.com>
2023-12-15 16:48:40 +0330

    useless comments deleted

--- getInput.py
+++ getInput.py
@@ -2,28 +2,6 @@ import struct
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# def loadTrace(fileName, numberOfTraces, traceSize):
-    
-#     with open(fileName, mode='rb') as file:
-#         fileContent = file.read()
-
-#     return [[struct.unpack("B" , fileContent[i:i + 1])[0] for i in range(j * traceSize, (j + 1) * traceSize)] for j in range(numberOfTraces)]
-
-# with open("traces.bin", mode='rb') as file:
-#     fileContent = file.read()
-    
-# y = [struct.unpack("B" , fileContent[i - 1:i])[0] for i in range(1, traceSize + 1)]
-# x = [i for i in range(traceSize)]
-
-
-# xpoints = np.array(x)
-# ypoints = np.array(y)
-
-# plt.plot(xpoints, ypoints)
-# plt.show()
-
-
 def loadData(fileName):
     
     result = []
@@ -39,7 +17,7 @@ def loadTrace(fname, trlen, start, length, n):
     
     with open(fname, 'rb') as myfile:
         
-        traces = np.zeros((n, length), dtype=np.uint8)
+        traces = np.zeros((n, length), dtype = np.uint8)
         
         for i in range(n):
             myfile.seek(start, 1)  # Seek from current position
