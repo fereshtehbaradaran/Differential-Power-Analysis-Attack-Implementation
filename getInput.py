1d7a327c097f9945b83b7278982dcc115cc78c54
fereshtehbaradaran <bara.frsht@gmail.com>
2023-12-15 14:59:31 +0330

    load trace function changed

--- getInput.py
+++ getInput.py
@@ -2,17 +2,13 @@ import struct
 import matplotlib.pyplot as plt
 import numpy as np
 
-# numberOfTraces  = 200
-# traceSize = 370 * 1000
 
-
-
-def loadTrace(fileName, numberOfTraces, traceSize):
+# def loadTrace(fileName, numberOfTraces, traceSize):
     
-    with open(fileName, mode='rb') as file:
-        fileContent = file.read()
+#     with open(fileName, mode='rb') as file:
+#         fileContent = file.read()
 
-    return [[struct.unpack("B" , fileContent[i:i + 1])[0] for i in range(j * traceSize, (j + 1) * traceSize)] for j in range(numberOfTraces)]
+#     return [[struct.unpack("B" , fileContent[i:i + 1])[0] for i in range(j * traceSize, (j + 1) * traceSize)] for j in range(numberOfTraces)]
 
 # with open("traces.bin", mode='rb') as file:
 #     fileContent = file.read()
@@ -36,3 +32,24 @@ def loadData(fileName):
         result = [[int('0x' + item, 16) for item in line.split()] for line in file]
     
     return result
+
+
+
+def loadTrace(fname, trlen, start, length, n):
+    
+    with open(fname, 'rb') as myfile:
+        
+        traces = np.zeros((n, length), dtype=np.uint8)
+        
+        for i in range(n):
+            myfile.seek(start, 1)  # Seek from current position
+            
+            if length + start > trlen:
+                t = np.fromfile(myfile, dtype = np.uint8, count = length - start)
+            else:
+                t = np.fromfile(myfile, dtype = np.uint8, count = length)
+                
+            myfile.seek(trlen - length - start, 1)  # Seek from current position
+            traces[i, :len(t)] = t  # In case t is shorter than length
+            
+    return traces
